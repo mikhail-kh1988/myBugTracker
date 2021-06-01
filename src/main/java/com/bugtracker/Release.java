ff5ebdacd735cb9b35ec478fb342bbd49344effa
Mikhail Khvashchevskiy <khvashchevskiy.mikhail@gmail.com>
2021-06-01 22:17:24 +0300

    Первый коммит.

---
+++ src/main/java/com/bugtracker/Release.java
@@ -0,0 +1,11 @@
+package com.bugtracker;
+
+/**
+ * @Author Khvahshchevskiy Mikhail
+ */
+public class Release {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/com/bugtracker/model/User.java
@@ -0,0 +1,8 @@
+package com.bugtracker.model;
+
+public class User {
+
+    private String login;
+    private String password;
+    private String description;
+}
